2726c85ea2b52406bbdff37d27e83649c3dbd800
yangzhen <1784054000@qq.com>
2016-01-29 15:19:52 +0800

    Datacache组件增加同步方法hasData，removeData，removeAll

--- doExtLib/do_DataCache_ISM.h
+++ doExtLib/do_DataCache_ISM.h
@@ -12,7 +12,10 @@
 
 @required
 //实现同步或异步方法，parms中包含了所需用的属性
+- (void)hasData:(NSArray *)parms;
 - (void)loadData:(NSArray *)parms;
+- (void)removeAll:(NSArray *)parms;
+- (void)removeData:(NSArray *)parms;
 - (void)saveData:(NSArray *)parms;
 
 @end
--- doExtLib/do_DataCache_SM.m
+++ doExtLib/do_DataCache_SM.m
@@ -18,6 +18,49 @@
 #pragma mark - 同步异步方法的实现
 
 //同步
+- (void)hasData:(NSArray *)parms
+{
+    NSDictionary *_dictParas = [parms objectAtIndex:0];
+    //参数字典_dictParas
+    doInvokeResult *_invokeResult = [parms objectAtIndex:2];
+    NSString *key = [doJsonHelper GetOneText:_dictParas :@"key" :@""];
+    if (key.length <= 0) {
+        [_invokeResult SetResultValue:[NSNull null]];
+        return;
+    }
+    id value =[[NSUserDefaults standardUserDefaults] objectForKey:key];
+    if (value == nil) {
+        [_invokeResult SetResultBoolean:NO];
+    }
+    else
+    {
+        [_invokeResult SetResultBoolean:YES];
+    }
+}
+- (void)removeAll:(NSArray *)parms
+{
+    doInvokeResult *_invokeResult = [parms objectAtIndex:2];
+    NSString *appDomain = [[NSBundle mainBundle]bundleIdentifier];
+    [[NSUserDefaults standardUserDefaults]removePersistentDomainForName:appDomain];
+    [_invokeResult SetResultBoolean:YES];
+}
+- (void)removeData:(NSArray *)parms
+{
+    NSDictionary *_dictParas = [parms objectAtIndex:0];
+    //参数字典_dictParas
+    doInvokeResult *_invokeResult = [parms objectAtIndex:2];
+    //自己的代码实现
+    NSString *key = [doJsonHelper GetOneText:_dictParas :@"key" :@""];
+    if (key.length <= 0) {
+        [_invokeResult SetResultValue:[NSNull null]];
+        return;
+    }
+    BOOL isExt = [[NSUserDefaults standardUserDefaults]objectIsForcedForKey:key];
+    if (isExt) {
+        [[NSUserDefaults standardUserDefaults] removeObjectForKey:key];
+    }
+    [_invokeResult SetResultBoolean:isExt];
+}
 - (void)loadData:(NSArray *)parms
 {
     NSDictionary *_dictParas = [parms objectAtIndex:0];
